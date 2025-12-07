76838ab7f228ac8341f8a087ff63d97ec6b1c694
EL FALEH JALAL <elfalehjalal.01@gmail.com>
2025-12-07 06:06:22 +0100

    Fix blank page issue

--- src/components/BookDetails.jsx
+++ src/components/BookDetails.jsx
@@ -1,1 +1,12 @@
-<div>Book Details Page for ISBN:</div>
+// src/components/BookDetails.jsx - CORRECT
+const BookDetails = () => {
+    return (
+        <div>
+        <h1>Book Details Component</h1>
+        {/* Your book details content here */}
+        </div>
+    );
+};
+
+// ⬇️⬇️⬇️ THIS LINE IS CRITICAL ⬇️⬇️⬇️
+export default BookDetails; // Must have this line
